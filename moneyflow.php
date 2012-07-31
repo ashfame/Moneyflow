4172b36ccd5de174fe721c34c942440bebfab9b9
Ashfame <mail@ashfame.com>
2012-07-31 23:30:11 +0530

    plugin url and license updated in plugin meta data

--- moneyflow.php
+++ moneyflow.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: Money Flow
-Plugin URI:
+Plugin URI: https://github.com/ashfame/Moneyflow
 Description: Take notes of how much money I need to give/take from others
 Author: Ashfame
 Author URI: http://blog.ashfame.com/
 Version: 0.1
-License: GPL
+License: GPLv2
 */
 
 if ( is_admin() )
